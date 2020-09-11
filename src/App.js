ed8d9d8b53dc21dec94507371dc262219fb18069
Donnie Mattingly <donniem@slalom.com>
2020-09-11 15:23:58 -0400

    add router and banner

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {getEspnFantasyData, getWeekScores, tester} from "./fantasy";
-import styled from 'styled-components';
+import styled, {css, keyframes} from 'styled-components';
 import {useInterval} from "./hooks";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 const ScoreRowContainer = styled.div`
   display: flex;
   flex-direction: row;
-  margin: 2em 1em;
+  margin: 0 0;
   justify-content: space-between;
-  
+  color: white;
   font-weight: bold;
-  color: black;
+  font-size: 1vw;
   
-  background: rgba(255, 255, 255, .6);
+  background: rgba(0, 0, 0, .65);
   border-radius: 1em;
 `
 
@@ -23,6 +29,7 @@ const GameContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-column-gap: 10px;
+  grid-row-gap: 10px;
 `
 
 const NameContainer = styled.div`
@@ -53,7 +60,7 @@ function GameScore({home, away}) {
     )
 }
 
-function App() {
+function LeagueScores() {
     const [games, setGames] = useState([]);
 
     useEffect(() => async () => {
@@ -71,6 +78,86 @@ function App() {
             {games.map(GameScore)}
         </ScoreRowContainer>
     );
+}
+
+const translate = keyframes`
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(-150vw, 0);
+  }
+`
+
+const bannerCss = css`
+  animation: ${translate} 30s linear infinite;
+  background: rgba(0, 0, 0, .65);
+  position: absolute;
+  width: 150vw;
+  display: grid;
+  grid-template-columns: repeat(${props => props.columns}, 1fr);
+`
+
+const BannerContainer = styled.div`
+  ${bannerCss};
+  margin-left: 0;
+`
+
+const BannerContainerOffset = styled.div`
+  ${bannerCss};
+  margin-left: 150vw;
+`
+
+function Banner({children}) {
+    const numChildren = React.Children.count(children);
+    return (
+        <div>
+            <BannerContainer columns={numChildren}>
+                {children}
+            </BannerContainer>
+
+            <BannerContainerOffset columns={numChildren}>
+                {children}
+            </BannerContainerOffset>
+        </div>
+    )
+}
+
+function UpdatesBanner() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => async () => {
+        const games = await getWeekScores();
+        setGames(games);
+    }, []);
+
+    useInterval(async () => {
+        const games = await getWeekScores();
+        setGames(games);
+    }, 10000);
+
+    return (
+        <div>
+            <Banner>
+                {games.map(GameScore)}
+            </Banner>
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/'>
+                    <LeagueScores/>
+                </Route>
+                <Route path='/updates'>
+                    <UpdatesBanner/>
+                </Route>
+            </Switch>
+        </Router>
+    )
 }
 
 export default App;
